905abc1cbc7d85c7e4353ded524344488e4a83c6
Dinesh <dineshm@2816@gmail.com>
2026-09-02 23:38:51 +0530

    feat: added new endpoint for coding assistant

--- src/ai/brain.py
+++ src/ai/brain.py
@@ -5,7 +5,7 @@ load_dotenv()
 import models.basemodel as basemodel
 from utils.settings import model_name
 
-def habit_maker(data):
+async def habit_maker(data):
 
     habit_agent = Agent(
         model_name,
@@ -13,19 +13,19 @@ def habit_maker(data):
         instructions='You are a helpful assistant that helps users create a list of habits based on their input. Please provide a list of habits in the output by their providing timeperiod or infromation like the habit cycle.',
     )
 
-    result = habit_agent.run_sync(data)
-    print(result.output.tips)
+    result = await habit_agent.run(data)
+    return result.output
 
 
-def coding_assistant(data):
+async def coding_assistant(data):
 
     coding_agent = Agent(
         model_name,
         output_type=basemodel.CodingEntry,
         instructions='You are a helpful assistant that helps users create coding skills and guidance based on their input. Please provide a valuable coding tips',
     )
-
-    result = coding_agent.run_sync(data)
-    print(result.output.tips)
+    
+    result = await coding_agent.run(data)
+    return result.output
 
 
--- src/main.py
+++ src/main.py
@@ -12,12 +12,17 @@ async def root():
     return {"Welcome to the personal agent"}
 
 @app.post("/habit-maker")
-async def habit_maker(data: str):
+async def habit_generator(prompt: str):
     from ai.brain import habit_maker
-    habit_maker(data)
-    return {"message": "Habit maker executed successfully."}
+    result = await habit_maker(prompt)
+    return {"message": "Habit maker executed successfully.", "result": result}
 
 
+@app.post("/coding-assistant")
+async def coding_trainer(prompt: str):
+    from ai.brain import coding_assistant
+    result = await coding_assistant(prompt)
+    return {"message": "Coding assistant executed successfully.", "result": result}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host=settings.host, port=settings.port, reload=True)
